e0d74c173cb102c60491f54a1a7e795316d244d4
tongcharlie <tongcharlie@gmail.com>
2016-11-13 23:47:53 -0800

    parsercg to feature vector sequence for training

--- parsercg.py
+++ parsercg.py
@@ -1,4 +1,30 @@
+''' Parses the .rcg file into a trainable format.
+
+    To add more features, list the feature name in OUTPUT_PLAYE_FEATURES and
+    uncomment the corresponding line in function parse().
+'''
 import sys
+
+DELIM = ','
+OUTPUT_FEATURES = [
+  'time',
+  'ball_pos_x',
+  'ball_pos_y',
+  'ball_vel_x',
+  'ball_vel_y',
+]
+OUTPUT_PLAYERS = [
+  'l1', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7', 'l8', 'l9', 'l10', 'l11',
+  'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'r10', 'r11',
+]
+OUTPUT_PLAYER_FEATURES = [
+  'pos_x',
+  'pos_y',
+  'vel_x',
+  'vel_y',
+  'angleBodyCommitted',
+]
+
 
 def parse(line):
   d = {}
@@ -9,84 +35,98 @@ def parse(line):
     tokens = group.strip().split(' ')
 
     if tokens[0] == 'show':
-      d['time']                     = int(tokens[1])
+      d['time']                     = tokens[1]
 
     elif tokens[0] == 'b':
-      d['ball_pos_x']               = float(tokens[1])
-      d['ball_pos_y']               = float(tokens[2])
-      d['ball_vel_x']               = float(tokens[3])
-      d['ball_vel_y']               = float(tokens[4])
+      d['ball_pos_x']               = tokens[1]
+      d['ball_pos_y']               = tokens[2]
+      d['ball_vel_x']               = tokens[3]
+      d['ball_vel_y']               = tokens[4]
 
     elif tokens[0] == 'l' or tokens[0] == 'r':
       p = d[tokens[0] + str(tokens[1])] = {}
-      p['side']                     = str(tokens[0])
-      p['unum']                     = int(tokens[1])
-      # p['playerTypeId']             = int(tokens[2])
-      # p['state']                    = str(tokens[3])
-      p['pos_x']                    = float(tokens[4])
-      p['pos_y']                    = float(tokens[5])
-      p['vel_x']                    = float(tokens[6])
-      p['vel_y']                    = float(tokens[7])
-      # p['angleBodyCommitted']       = float(tokens[8])
-      # p['angleNeckCommitted']       = float(tokens[9])
+      # p['side']                     = tokens[0]
+      # p['unum']                     = tokens[1]
+      # p['playerTypeId']             = tokens[2]
+      # p['state']                    = tokens[3]
+      p['pos_x']                    = tokens[4]
+      p['pos_y']                    = tokens[5]
+      p['vel_x']                    = tokens[6]
+      p['vel_y']                    = tokens[7]
+      p['angleBodyCommitted']       = tokens[8]
+      # p['angleNeckCommitted']       = tokens[9]
       # if len(tokens) > 10:
-      #   p['arm_dest_X']             = float(tokens[10])
-      #   p['arm_dest_Y']             = float(tokens[11])
+      #   p['arm_dest_X']             = tokens[10]
+      #   p['arm_dest_Y']             = tokens[11]
 
     # elif tokens[0] == 'v':
-    #   p['highQuality']              = str(tokens[1])
-    #   p['visibleAngle']             = float(tokens[2])
+    #   p['highQuality']              = tokens[1]
+    #   p['visibleAngle']             = tokens[2]
 
     # elif tokens[0] == 's':
-    #   p['stamina']                  = float(tokens[1])
-    #   p['effort']                   = float(tokens[2])
-    #   p['recovery']                 = float(tokens[3])
-    #   p['staminaCapacity']          = float(tokens[4])
+    #   p['stamina']                  = tokens[1]
+    #   p['effort']                   = tokens[2]
+    #   p['recovery']                 = tokens[3]
+    #   p['staminaCapacity']          = tokens[4]
 
     # elif tokens[0] == 'f':
-    #   p['getFocusTarget_side']      = str(tokens[1])
-    #   p['getFocusTarget_unum']      = int(tokens[2])
+    #   p['getFocusTarget_side']      = tokens[1]
+    #   p['getFocusTarget_unum']      = tokens[2]
 
     # elif tokens[0] == 'c':
-    #   p['kickCount']                = str(tokens[1])
-    #   p['dashCount']                = int(tokens[2])
-    #   p['turnCount']                = int(tokens[3])
-    #   p['catchCount']               = str(tokens[4])
-    #   p['moveCount']                = float(tokens[5])
-    #   p['turnNeckCount']            = float(tokens[6])
-    #   p['changeViewCount']          = float(tokens[7])
-    #   p['sayCount']                 = float(tokens[8])
-    #   p['tackleCount']              = float(tokens[9])
-    #   p['arm_getCounter']           = float(tokens[10])
-    #   p['attentiontoCount']         = float(tokens[11])
+    #   p['kickCount']                = tokens[1]
+    #   p['dashCount']                = tokens[2]
+    #   p['turnCount']                = tokens[3]
+    #   p['catchCount']               = tokens[4]
+    #   p['moveCount']                = tokens[5]
+    #   p['turnNeckCount']            = tokens[6]
+    #   p['changeViewCount']          = tokens[7]
+    #   p['sayCount']                 = tokens[8]
+    #   p['tackleCount']              = tokens[9]
+    #   p['arm_getCounter']           = tokens[10]
+    #   p['attentiontoCount']         = tokens[11]
 
   return d
 
 
+def featurestring():
+  string = '#' + DELIM.join(OUTPUT_FEATURES)
+  for player in OUTPUT_PLAYERS:
+    for feature in OUTPUT_PLAYER_FEATURES:
+      string += DELIM + player + '.' + feature
+  return string
+
+
 def tostring(d):
-  # TODO
-  # convert dict d into a one-line string to be written to file for training
-  return str(d)
+  string = DELIM.join([d[f] for f in OUTPUT_FEATURES])
+  for player in OUTPUT_PLAYERS:
+    for feature in OUTPUT_PLAYER_FEATURES:
+      string += DELIM + d[player][feature]
+  return string
+
+
+def parse_rcg(in_filename):
+  with open(in_filename, 'r') as in_file:
+    sequence = []
+    for line in in_file:
+      if line.startswith('(show '):
+        d = parse(line)
+        if int(d['time']) > len(sequence):
+          sequence.append(tostring(d))
+    return sequence
+
+
+def write_sequence(sequence, out_filename, write_header=True):
+  with open(out_filename, 'w') as out_file:
+    if write_header:
+      out_file.write(featurestring() + '\n')
+    for t in sequence:
+      out_file.write(t + '\n')
 
 
 if __name__ == '__main__':
   if len(sys.argv) < 3:
     print 'Usage:  %s <in_file.rcg> <out_file.txt>' % sys.argv[0]
     sys.exit(-1)
-
-  in_file  = open(sys.argv[1], 'r')
-  out_file = open(sys.argv[2], 'w')
-
-  blacklist_time = 0
-  for line in in_file:
-    if line.startswith('(show '):
-      d = parse(line)
-      if d['time'] != blacklist_time:
-        str_d = tostring(d)
-        out_file.write(str_d + '\n')
-
-    elif line.startswith('(playmode '):
-      blacklist_time = int(line.split(' ')[1])
-
-    else:
-      print line.strip()
+  sequence = parse_rcg(sys.argv[1])
+  write_sequence(sequence, sys.argv[2])
